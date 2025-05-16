c43cb31d30b32ee7cf6c74fcb51940a962b072c3
Shyam-GK <shyamgokulkrish@gmail.com>
2025-05-16 13:24:37 +0530

    Added server and Redis config files

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require("./app");
+const pool = require("./config/db");
+const redisClient = require("./config/redis");
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+  try {
+    await pool.connect();
+    console.log("🔥🔥🔥PostgreSQL connected");
+
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Startup error:", err);
+    process.exit(1);
+  }
+})();
